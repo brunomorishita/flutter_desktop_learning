69c13d424b0235a066823b45c0507b732414c8d4
brunomorishita <brunomorishita@outlook.com>
2022-02-17 21:03:09 -0300

    Added initial support for using openal-soft native together with ffigen

--- lib/record_page.dart
+++ lib/record_page.dart
@@ -1,17 +1,34 @@
+import 'dart:ffi';
 import 'dart:io';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/foundation.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:styled_widget/styled_widget.dart';
 import 'package:audio_recorder_nullsafety/audio_recorder_nullsafety.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'native_openal_soft_bindings.dart' as oal;
 
 enum RecordingState { Idle, Start }
+
+String _getPath() {
+  final openalSoftWrapperPath = Directory.current.absolute.path;
+  var path = p.join(openalSoftWrapperPath, 'build\\windows\\native_lib');
+
+  String mode = kDebugMode ? 'Debug' : 'Release';
+  path = p.join(path, mode, 'openal-soft-wrapper.dll');
+
+  print('path: $path');
+  return path;
+}
 
 class RecordPage extends StatelessWidget {
   RecordingState recordingState = RecordingState.Idle;
   late String appDocPath;
   String audioFileName = "record.wav";
   late String audioFilePath;
+
+  final openalSoft = oal.OpenAlSoft(DynamicLibrary.open(_getPath()));
 
   RecordPage() {
     initializer();
@@ -46,6 +63,7 @@ class RecordPage extends StatelessWidget {
               await AudioRecorder.start(
                   path: audioFilePath, audioOutputFormat: AudioOutputFormat.WAV);
               recordingState = RecordingState.Start;
+              openalSoft.init();
               break;
             }
           case RecordingState.Start:
---
+++ windows/native_lib/native_openal_soft.h
@@ -0,0 +1,14 @@
+#ifndef FLUTTER_DESKTOP_LEARNING_NATIVE_OPENAL_SOFT_H
+#define FLUTTER_DESKTOP_LEARNING_NATIVE_OPENAL_SOFT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void init();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //FLUTTER_DESKTOP_LEARNING_NATIVE_OPENAL_SOFT_H
