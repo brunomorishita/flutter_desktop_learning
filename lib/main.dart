d7dfd6e6623484901bcbbf590548a1810e6317a6
brunomorishita <brunomorishita@outlook.com>
2022-03-05 08:49:10 -0300

    Trying to add locale support

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:fluent_ui/fluent_ui.dart';
 import 'package:system_theme/system_theme.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'pages/home_page.dart';
 
 void main() {
@@ -10,7 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FluentApp(
-        title: 'Voice Recording',
+        onGenerateTitle: (context) {
+          return AppLocalizations.of(context)!.appTitle;
+        },
+        localizationsDelegates: [
+          AppLocalizations.delegate,
+        ],
+        supportedLocales: AppLocalizations.supportedLocales,
         theme: ThemeData(accentColor: SystemTheme.accentInstance.accent.toAccentColor()),
         home: HomePage());
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:fluent_ui/fluent_ui.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_trial/pages/record_page.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'audio_file_page.dart';
 import '../stores/home_page_store.dart';
@@ -14,14 +15,14 @@ class HomePage extends StatelessWidget {
     return Observer(
         builder: (_) => NavigationView(
               appBar: NavigationAppBar(
-                title: const Text("Voice Recording 2"),
+                title: Text(AppLocalizations.of(context)!.appBarTitle),
                 automaticallyImplyLeading: false,
               ),
               pane: NavigationPane(
                 selected: _homePageStore.index,
                 onChanged: (i) => _homePageStore.setIndex(i),
                 displayMode: PaneDisplayMode.auto,
-                items: _getPanelItems(),
+                items: _getPanelItems(context),
               ),
               content: NavigationBody(
                 index: _homePageStore.index,
@@ -30,15 +31,15 @@ class HomePage extends StatelessWidget {
             ));
   }
 
-  List<NavigationPaneItem> _getPanelItems() {
+  List<NavigationPaneItem> _getPanelItems(BuildContext context) {
     return <NavigationPaneItem>[
       PaneItem(
         icon: FaIcon(FontAwesomeIcons.microphone),
-        title: const Text('Record'),
+        title: Text(AppLocalizations.of(context)!.panelItemRecord),
       ),
       PaneItem(
         icon: FaIcon(FontAwesomeIcons.folder),
-        title: const Text('Saved Files'),
+        title: Text(AppLocalizations.of(context)!.panelItemFiles),
       ),
     ];
   }
